10f1c75359cf06af1b21e857225862c130c61d38
TRITONKOR <sasalahotskij@gmail.com>
2025-11-06 14:32:47 +0200

    replace react-router-dom with react-router

--- src/components/AdminPanel/PanelSwitch/PanelSwitch.tsx
+++ src/components/AdminPanel/PanelSwitch/PanelSwitch.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { Button } from '../../../components/ui/Button/Button';
 import { useAuth } from '../../../context/AuthContext';
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { Button } from '../../components/ui/Button/Button';
 import { useAuth } from '../../context/AuthContext';
 import PanelSwitch from '../AdminPanel/PanelSwitch/PanelSwitch';
